33a665d9d08eb7362874a802f1843d391458d6f6
Dane LeGassick <danelegassick@gmail.com>
2023-03-01 17:19:33 -0500

    Initial Commit

---
+++ Note Taking/Note_TakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_TakingApp.swift
+//  Note Taking
+//
+//  Created by dane legassick on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Note_TakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Note TakingUITests/Note_TakingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Note_TakingUITestsLaunchTests.swift
+//  Note TakingUITests
+//
+//  Created by dane legassick on 3/1/23.
+//
+
+import XCTest
+
+final class Note_TakingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
