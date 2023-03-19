eeac7c6b7bfe0bc7b32fbcaa24e9c2f6275d8906
Dane LeGassick <danelegassick@gmail.com>
2023-03-19 15:01:08 -0400

    Final commit

--- Note Taking/HomeView.swift
+++ Note Taking/HomeView.swift
@@ -8,114 +8,63 @@
 import SwiftUI
 
 struct HomeView: View{
-    @EnvironmentObject var notesVM: NotesViewModel
-    @FetchRequest(sortDescriptors: []) var notes: FetchedResults<Note>
-    
     @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.dateAdded),
+        SortDescriptor(\.organizer, order: .reverse)
+    ]) var notes: FetchedResults<Note>
+    
+    @State private var showingAddScreen = false
+
     
     var body: some View {
-        
-        
-        ScrollView {
-            VStack {
-                HStack {
-                    Text("\(notes.count) \(notes.count > 1 ? "Notes" : "Note")")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .opacity(0.7)
-                    
-                    Spacer()
-                }
-                
-                LazyVGrid(columns: [GridItem(.adaptive(minimum:160), spacing: 15)], spacing: 15) {
-                    ForEach(notes, id: \.self) { note in
-                        VStack {
-                            AsyncImage(url: URL(string: note.image ?? "https://www.incimages.com/uploaded_files/image/1920x1080/getty_655998316_2000149920009280219_363765.jpg")) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .overlay(alignment: .bottom) {
-                                        Text(note.title ?? "Unknown name")
-                                            .font(.headline)
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.white)
-                                            .frame(maxWidth: 136)
-                                            .padding()
-                                            .shadow(color:.black, radius: 3, x:0, y:0)
-                                    }
-                            } placeholder: {
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame (width: 40, height: 40, alignment: .center)
-                                    .foregroundColor(.white.opacity(0.7))
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                
+        NavigationView {
+            List {
+                ForEach(notes) { note in
+                    NavigationLink {
+                        TestDetailView(note: note)
+                    } label: {
+                        HStack {
+                            TestOrganizerView(organizer: note.organizer ?? "Misc")
+                                .font(.largeTitle)
+                            
+                            VStack(alignment: .leading) {
+                                Text(note.title ?? "Unknown Title")
+                                    .font(.headline)
                             }
                         }
-                        .frame (width: 160, height: 217, alignment: .top)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
-                        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                        .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
-                        
                     }
                 }
-                .padding(.top)
-                
+                .onDelete(perform: deleteNotes)
             }
-            .padding(.horizontal)
-            
+                .navigationTitle("Notate")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Note", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddTestView()
+                }
         }
         
+// ++++++++++ This is the current design beneath ++++++++++++++++++++++++
+//        TabBar()
+    }
+    
+    func deleteNotes(at offsets: IndexSet) {
+        for offset in offsets {
+            let note = notes[offset]
+            moc.delete(note)
+        }
+        
+        try? moc.save()
     }
 }
-        
-        
-        
-        
-        
-//        ScrollView {
-//            ForEach(notes, id: \.self) { note in
-////                Text(note.title ?? "Unknown name")
-//                //                Text("\(note.age) years old")
-//                VStack {
-//                    AsyncImage(url: URL(string: note.image ?? "https://www.incimages.com/uploaded_files/image/1920x1080/getty_655998316_2000149920009280219_363765.jpg")) { image in
-//                        image
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fill)
-//                            .overlay(alignment: .bottom) {
-//                                Text(note.title ?? "Unknown name")
-//                                    .font(.headline)
-//                                    .fontWeight(.bold)
-//                                    .foregroundColor(.white)
-//                                    .frame(maxWidth: 136)
-//                                    .padding()
-//                                    .shadow(color:.black, radius: 3, x:0, y:0)
-//                            }
-//                    } placeholder: {
-//                        Image(systemName: "photo")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .frame (width: 40, height: 40, alignment: .center)
-//                            .foregroundColor(.white.opacity(0.7))
-//                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-//
-//                    }
-//                }
-//                .frame (width: 160, height: 217, alignment: .top)
-//                .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
-//                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-//                .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
-//            }
-//        }
-//    }
-//}
-    
-    
-struct HomeView_Previews: PreviewProvider{
-        static var previews: some View {
-            HomeView()
-                .environmentObject(NotesViewModel())
-        }
-    }
-
